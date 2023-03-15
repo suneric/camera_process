f43316a571941297a6614ab68b9863b2239eef14
suneric <syf_more@163.com>
2023-03-15 15:33:14 -0400

    add detection

--- auto_start.sh
+++ auto_start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://192.168.1.7:11311
-export ROS_IP=192.168.1.17
 export ROS_HOSTNAME=nano
+export ROS_MASTER_URI=http://192.168.1.7:11311
 
 source /opt/ros/melodic/setup.bash
 source /home/jetson/catkin_ws/devel/setup.bash
